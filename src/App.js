795e032eb10769510e63e8e2e6d881e3719b2039
sshinohs <sshinohs@gmail.com>
2022-04-19 19:18:13 +0900

    클론코딩.. 영화앱 마무리

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import Home from './routes/Home';
 import './App.css';
 import {HashRouter, Route} from 'react-router-dom';
 import About from './routes/About';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
 function App() {
   return (
         <HashRouter>
+            <Navigation/>
             <Route path="/" exact={true} component={Home}/>
             <Route path="/about" component={About}/>
+            <Route path="/movie-detail" component={Detail}/>
         </HashRouter>
       );
 }
